6a5817c78321425318e721e967143b87956c651b
Humaira Tasnim Ridita <56226099+Humaira1227@users.noreply.github.com>
2022-12-20 21:47:33 +0600

    Add files via upload

---
+++ Registration.java
@@ -0,0 +1,38 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import jakarta.servlet.annotation.WebServlet;
+
+@WebServlet("/Registration")
+public class Registration {
+    public boolean UserRegister(String user_name, String user_email, String user_password, String user_phone, String user_address){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/company", "root", "novemberrain");
+            
+            String query="INSERT INTO company.users(user_name, user_email, user_password, user_phone, user_address) VALUES(?,?,?,?,?)";
+            PreparedStatement pst= conn.prepareStatement(query);
+            pst.setString(1, user_name);
+            pst.setString(2, user_email);
+            pst.setString(3, user_password);
+            pst.setString(4, user_phone);
+            pst.setString(5, user_address);
+            
+            if(pst.executeUpdate() > 0){
+                return true;
+            }
+            pst.close();
+            conn.close();
+            
+        }
+        catch (ClassNotFoundException | SQLException ex) 
+        {
+            System.out.println(ex);
+        }
+        return false;
+    }
+    
+    
+}
